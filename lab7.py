bc2b2852870c599f3ffdbbfbf2f9f549683143c7
Onnesok <ratulhasan9464@gmail.com>
2022-12-07 23:18:14 +0600

    Questions

--- lab7.py
+++ lab7.py
@@ -1,25 +1,57 @@
+# def idx(arr):
+#     mx = 0
+#     mn = 0
+#     for i in range(len(arr)):
+#         if arr[i] > mx:
+#             mx = arr[i]
+#         elif arr[i] < mn:
+#             mn = arr[i]
+    
+#     aux = [0]*(mx+1)
+#     for i in range(len(arr)):
+#         c = arr[i]
+#         aux[c] += 1
+#     for i in range(len(aux)):
+#         for j in range(aux[i]):
+#             print(i, end = " ")
+    
+# x = [1, 2, 5, 7, 6, 0, 2]
+# idx(x)
 
-def keyidx(a):
-    mx = a[0]
-    mn = a[0]
-    for i in range(len(a)):
-        if mx < a[i]:
-            mx = a[i]
-        if mn > a[i]:
-            mn = a[i]
-    aux = [0] * (mx + (mn * -1) + 1)
-    val = [0] * (mx + (mn * -1) + 1)
-    for i in range(len(a)):
-        if a[i] < 0:
-            aux[a[i]-mn] += 1
-        else:
-            aux[a[i]-mn] += 1
-    for i in range(len(aux)):
-        indx = a[i] - mn # aux idx position
-        cash = aux[indx]
-        print(indx, a[i], cash)
-        val[i] = aux[indx] * a[i]
-    return val
+class keyidx:
+    def __init__(self, arr):
+        mx = 0
+        mn = 0
+        for i in range(len(arr)):
+            if arr[i] > mx:
+                mx = arr[i]
+            elif arr[i] < mn:
+                mn = arr[i]
+                self.posmn = abs(mn)
+        self.aux = [0] * (mx + abs(mn) + 1)
+        for i in range(len(arr)):
+            c = arr[i]
+            if c < 0:
+                self.aux[c+self.posmn] += 1
+            else:
+                self.aux[c+self.posmn] += 1
+        print(self.aux)
+        # for  i in range(len(self.aux)):
+        #     for j in range(self.aux[i]):
+        #         print(i - self.posmn, end=" ")
+                
+    def search(self, val):
+        for i in range(len(self.aux)):
+            if val+self.posmn <= len(self.aux) and self.aux[val + self.posmn] >= 1 and val+self.posmn >= 0:
+                return True
+            else:
+                return False
+    def sort(self):
+        for i in range(len(self.aux)):
+            for j in range(self.aux[i]):
+                print(i - self.posmn, end=" ")
 
-a = [1,2,3,4,-2, -2,6,-1,5]
-print(keyidx(a))
+x = [5, 7, -7, -5, -6, -3, -3]         
+a = keyidx(x)
+print(a.search(-8))
+a.sort()
