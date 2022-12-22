ac2e9f2c091508a1755982fada48b1b0756d8190
Onnesok <ratulhasan9464@gmail.com>
2022-12-23 01:04:58 +0600

    lab_final( recursion )

---
+++ lab_final.py
@@ -0,0 +1,21 @@
+def array(n):
+    i = 1
+    arr = ["-1"]*n
+    while i <= n:
+        arr[i-1] = i
+        i+= 1
+    return arr
+
+def game(n, k, start = 0):
+    if type(n) == int:
+        temp = array(n)
+        n = temp
+    if len(n) == 1:
+        return n[0]
+    
+    start = (start + (k-1))%len(n)
+    n.pop(start)
+    return game(n, k, start = start)
+    
+
+print(game(5, 2))
